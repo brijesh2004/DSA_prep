a814681ea3a02e44d6860cea2ad02e7e37c9483a
brijesh2004 <brskumar0102@gmail.com>
2025-07-10 13:10:04 +0530

    suffle interger without extra space but more time

--- recursion_and_backtracking/suffle_integer.cpp
+++ recursion_and_backtracking/suffle_integer.cpp
@@ -1,6 +1,18 @@
 class Solution {
 
   public:
+
+     void shuffleArray(int arr[], int n) {
+        // Your code goes here
+        int j = n/2;
+        for(int i=0;i<n;i+=2,j++){
+            int k = j;
+            while(k>i+1){
+                swap(arr[k] , arr[k-1]);
+                k--;
+            }
+        }
+    }
     void shuffleArray(int arr[], int n) {
         // Your code goes here
         
